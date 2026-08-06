9012e9b3c38b56e6c497e457f316cdc22374ede1
NallapaneniManjula <manjulachowdary1304@gmail.com>
2026-08-06 17:57:45 +0530

    Add files via upload

---
+++ Longest_Substring_Without_Repeating_Characters/solution.py
@@ -0,0 +1,11 @@
+s=input()
+max_len=0
+for i in range(len(s)):
+    temp=[]
+    for j in range(i,len(s)):
+        if s[j] in temp:
+            break
+        temp.append(s[j])
+    if len(temp)>max_len:
+        max_len=len(temp)
+print(max_len)
